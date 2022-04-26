ec4cc88803bce1a5cffc3e15fdcea939c7954d3f
dasom <50973005+dasom222g@users.noreply.github.com>
2022-04-26 18:13:30 +0900

    Add counter

--- pages/counter.tsx
+++ pages/counter.tsx
@@ -1,3 +1,11 @@
-export default function counter() {
-  return `<h3>Counter</h3>`
+import { useState } from "react"
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h3>Counter {count}</h3>
+      <button type="button" onClick={() => setCount(prev => prev + 1)}>+</button>
+    </>
+  )
 }
