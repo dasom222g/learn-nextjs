9a6466cc4e94db0535f7ba7cdf32675545688945
dasom <50973005+dasom222g@users.noreply.github.com>
2022-04-27 01:28:41 +0900

    Add useRouter

--- components/Navigator.tsx
+++ components/Navigator.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Navigator() {
+  const router = useRouter()
+  console.log('router', router)
   return (
     <nav style={{margin:'0 -4px'}}>
       <Link href="/">
         <a className="link" style={{padding: '2px 4px'}}>Home</a>
       </Link>
-      <Link href="about">
+      <Link href="/about">
         <a className="link" style={{padding: '2px 4px'}}>About</a>
       </Link>
     </nav>
